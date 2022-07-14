c37cbe3f9162df4b368e99d9af5ccf22856af2b7
Fireman <vaclav@koterec.cz>
2022-07-14 21:47:45 +0200

    Added placeholders for translations

--- extension.js
+++ extension.js
@@ -13,6 +13,8 @@ const Domain = Gettext.domain('messages');
 // function is often aliased as `_()`
 const _ = Domain.gettext;
 const ngettext = Domain.ngettext;
+const pgettext = Domain.pgettext;
+const npgettext = Domain.npgettext; // translations with context
 
 let dateMenu = null;
 let settings = null;
@@ -23,38 +25,10 @@ function FuzzyClock() {
 	this.init();
 }
 
+//settings set org.gnome.desktop.interface clock-show-seconds true
+//this._settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.surf');
 FuzzyClock.prototype = {
 	init: function() {
-		this.hour_format = [
-			_("%0 o'clock"),
-			_("Five past %0"),
-			_("Ten past %0"),
-			_("Quarter past %0"),
-			_("Twenty past %0"),
-			_("Twenty five past %0"),
-			_("Half past %0"),
-			_("Twenty five to %1"),
-			_("Twenty to %1"),
-			_("Quarter to %1"),
-			_("Ten to %1"),
-			_("Five to %1"),
-			_("%1 o'clock")
-		];
-		this.hour_names = [
-			_("Twelve"),
-			_("One"),
-			_("Two"),
-			_("Three"),
-			_("Four"),
-			_("Five"),
-			_("Six"),
-			_("Seven"),
-			_("Eight"),
-			_("Nine"),
-			_("Ten"),
-			_("Eleven"),
-			_("Twelve")
-		];
 		this.month_format = [
 			_("Beginning of %0"),
 			_("Early %0"),
@@ -78,13 +52,129 @@ FuzzyClock.prototype = {
 		];
 	},
 
-	time: function(now) {
-		let hours = now.get_hour();
-		return this.hour_format[Math.round(now.get_minute() / 5)]
-			.replace("%0", this.hour_names[hours >= 12 ? hours - 12 : hours])
-			.replace("%1", this.hour_names[hours + 1 >= 12 ? hours + 1 - 12 : hours + 1]);
+	/**
+	 * @param hour int
+	 * @param minute int
+	 * @returns {string}
+	 */
+	_formatHour: function(hour, minute) {
+		let minutesFormat = {
+			0: ngettext("X:00", "X:00", hour), //%is o'clock
+			5: _("X:05"), //Five past %past
+			10:_("X:10"), //Ten past %past
+			15:_("X:15"), //Quarter past %past
+			20:_("X:20"), //Twenty past %past
+			25:_("X:25"), //Twenty five past %past
+			30:_("X:30"), //Half past %past
+			35:_("X:35"), //Twenty five to %to
+			40:_("X:40"), //Twenty to %to
+			45:_("X:45"), //Quarter to %to
+			50:_("X:50"), //Ten to %to
+			55:_("X:55"), //Five to %to
+			60:ngettext("X:60", "X:60", hour) //%will o'clock
+		};
+
+		return minutesFormat[minute];
 	},
 
+	/**
+	 * @param hour int
+	 * @param context string
+	 * @returns {string}
+	 */
+	formatHourName: function(hour, context) {
+		//translations
+		pgettext("Is", "Zero");
+		pgettext("Past", "Zero");
+		pgettext("To", "Zero");
+		pgettext("Will be", "Zero");
+		pgettext("Is", "One");
+		pgettext("Past", "One");
+		pgettext("To", "One");
+		pgettext("Will be", "One");
+		pgettext("Is", "Two");
+		pgettext("Past", "Two");
+		pgettext("To", "Two");
+		pgettext("Will be", "Two");
+		pgettext("Is", "Three");
+		pgettext("Past", "Three");
+		pgettext("To", "Three");
+		pgettext("Will be", "Three");
+		pgettext("Is", "Four");
+		pgettext("Past", "Four");
+		pgettext("To", "Four");
+		pgettext("Will be", "Four");
+		pgettext("Is", "Five");
+		pgettext("Past", "Five");
+		pgettext("To", "Five");
+		pgettext("Will be", "Five");
+		pgettext("Is", "Six");
+		pgettext("Past", "Six");
+		pgettext("To", "Six");
+		pgettext("Will be", "Six");
+		pgettext("Is", "Seven");
+		pgettext("Past", "Seven");
+		pgettext("To", "Seven");
+		pgettext("Will be", "Seven");
+		pgettext("Is", "Eight");
+		pgettext("Past", "Eight");
+		pgettext("To", "Eight");
+		pgettext("Will be", "Eight");
+		pgettext("Is", "Nine");
+		pgettext("Past", "Nine");
+		pgettext("To", "Nine");
+		pgettext("Will be", "Nine");
+		pgettext("Is", "Ten");
+		pgettext("Past", "Ten");
+		pgettext("To", "Ten");
+		pgettext("Will be", "Ten");
+		pgettext("Is", "Eleven");
+		pgettext("Past", "Eleven");
+		pgettext("To", "Eleven");
+		pgettext("Will be", "Eleven");
+		pgettext("Is", "Twelve");
+		pgettext("Past", "Twelve");
+		pgettext("To", "Twelve");
+		pgettext("Will be", "Twelve");
+
+		let hourNames = {
+			0:pgettext(context, "Zero"), // translated to Twelve
+			1:pgettext(context, "One"),
+			2:pgettext(context, "Two"),
+			3:pgettext(context, "Three"),
+			4:pgettext(context, "Four"),
+			5:pgettext(context, "Five"),
+			6:pgettext(context, "Six"),
+			7:pgettext(context, "Seven"),
+			8:pgettext(context, "Eight"),
+			9:pgettext(context, "Nine"),
+			10:pgettext(context, "Ten"),
+			11:pgettext(context, "Eleven"),
+			12:pgettext(context, "Twelve")
+		};
+
+		return hourNames[hour];
+	},
+
+	/**
+	 * @param now GLib.DateTime
+	 * @returns {string}
+	 */
+	time: function(now) {
+		let hours = now.get_hour();
+		let minutes = Math.round(now.get_minute() / 5) * 5;
+		return this._formatHour(hours, minutes)
+			.replace("%is", this.formatHourName(hours >= 12 ? hours - 12 : hours, "Is"))
+			.replace("%past", this.formatHourName(hours >= 12 ? hours - 12 : hours, "Past"))
+			.replace("%to", this.formatHourName(hours + 1 >= 12 ? hours + 1 - 12 : hours + 1, "To"))
+			.replace("%will", this.formatHourName(hours + 1 >= 12 ? hours + 1 - 12 : hours + 1, "Will be"))
+			+ " " + now.get_minute();
+	},
+
+	/**
+	 * @param now GLib.DateTime
+	 * @returns {string}
+	 */
 	date: function(now) {
 		let month = now.get_month();
 		let day = now.get_day_of_month();
